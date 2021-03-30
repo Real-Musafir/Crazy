69051ea1996b341fa624895ca44aca30565289b3
Al Shahadath <shctg1@gmail.com>
2021-03-30 16:39:47 +0600

    initial commit

--- container/Cart.js
+++ container/Cart.js
@@ -49,7 +49,7 @@ class Cart extends Component {
       backgroundColor: '#fff',
       padding: 10,
     }
-    const rawData = {
+    const dataRow = {
       flexDirection: 'row',
       justifyContent: 'space-between',
     }
